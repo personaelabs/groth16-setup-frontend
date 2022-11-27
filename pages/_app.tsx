b630ebd9be980e65a007bd1572f09ed490ffd1d7
lsankar4033 <lsankar4033@gmail.com>
2022-11-26 19:21:01 -0800

    Add prev zkey, collect entropy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 const snarkjs = require("snarkjs");
-
 console.log(snarkjs.zKey.contribute);
 
 export default function App({ Component, pageProps }: AppProps) {
